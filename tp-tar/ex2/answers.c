1f34587d1b704b2e157d3700fff376d409f33868
Eyphah <99732146+Eyphah@users.noreply.github.com>
2024-10-07 18:41:37 +0200

    Update answers.c

--- tp-tar/ex2/answers.c
+++ tp-tar/ex2/answers.c
@@ -26,23 +26,23 @@ int list_archive(const char *archive_name)
     return 0;
 }
 
-int generate_offsets(const char *archive_name, struct loffsets *lo) //en gros on lit les entetes et on donne leur position en leur retirant leur taille puisqu'on les lit jusu'au bout avant de retourner la valeur
+int generate_offsets(const char *archive_name, struct loffsets *lo) //we count the amount of files within the archive and for each of them put their header offset (where their header is) in an array whos index matches the file number
 {
-    FILE *f = fopen(archive_name, "rb"); //rb pr lire fichiers bin
+    FILE *f = fopen(archive_name, "rb"); //rb pr lire fichiers bin / rb to read binary files
     if(f==NULL){
         return -1;
     }
 
-    lo->n_files=0; //initialiser à chaque appel sinon on peut commencer à compter à partir d'une valeur indéfinie
+    lo->n_files=0; //initialisation to avoid the values stacking if multiple function calls
     struct header h;
-    while(fread(&h, sizeof(struct header), 1,f)>0){ //on lit 1 par 1 les bytes d'un entête
-        if(lo->n_files >= MAX_FILES){ //check si trop de fichiers
+    while(fread(&h, sizeof(struct header), 1,f)>0){ //read the header and store it in a header struct
+        if(lo->n_files >= MAX_FILES){ //check if too many files
             fclose(f);
             return -1;
         }
-        lo->offset[lo->n_files ] = ftell(f) - sizeof(struct header); //position de l'entête (enfait on lit jusqu'à la fin de l'entête dc pour avoir le début on lui soustrait sa taille)
+        lo->offset[lo->n_files ] = ftell(f) - sizeof(struct header); //pos of the header ( we read until the end of it so we susbtract its size for its first position)
         lo->n_files++;
-        fseek(f,h.size,SEEK_CUR); //on utilise header pour pouvoir keep track de la taille des fichiers
+        fseek(f,h.size,SEEK_CUR); 
     }
     fclose(f);
     return 0;
@@ -58,22 +58,22 @@ int print_file(const char *archive_name, struct loffsets *lo, int n)
         return -2;
     }
 
-    FILE *f = fopen(archive_name, "rb"); //réouvre car generate_offsets close le fichier
+    FILE *f = fopen(archive_name, "rb"); //reopen bc generate_offsets closes the file
     if(f==NULL){
         return -1;
     }
     int offset = lo->offset[n];
     int size = (n+1<lo->n_files)?(lo->offset[n+1]-offset):(lo->offset[n]+sizeof(struct header));
     
-    fseek(f, offset, SEEK_SET); //on se positionne après l'entête du fichier
+    fseek(f, offset, SEEK_SET); //putting cursor at beginning of file
     
     char *fileContent = (char *)malloc(size*sizeof(char));
     if (fileContent == NULL){
         fclose(f);
-        return -1; //echec d'allocation donc on termine
+        return -1; //memory allocation failed so stopping process
     }
-    fread(fileContent, sizeof(char),size,f); //on lit l'entièreté du fichier
-    output_bytes(fileContent,size);
+    fread(fileContent, sizeof(char),size,f); //read file number n
+    output_bytes(fileContent,size); //write fileContent
     free(fileContent);
     fclose(f);
     return 0;
